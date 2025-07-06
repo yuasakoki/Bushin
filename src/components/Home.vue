2ad7490e74e1df9195183b4fd7ccc58cb6d34067
yuasakoki <autoshopsanshin@gmail.com>
2025-07-06 15:01:37 +0900

    ホーム画面に各ページに遷移するリンク表示

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
-    <h1>武神会</h1>
-  </div>
+    <header>
+      <p>ホーム</p> 
+      <p>こんにちは、ユーザーさん！</p> 
+      <h1>武神会</h1>
+      <h1>全日本空手形大会</h1> 
+    </header>
+    <body>
+      <div class="home-button-list">
+        <button @click="onChangePage('about')">一般女子</button>
+        <button @click="onChangePage('about')">男子：上級</button>
+        <button @click="onChangePage('about')">男子：中級</button>
+        <button @click="onChangePage('about')">男子：初級</button>
+        <button @click="onChangePage('about')">女子：上級</button>
+        <button @click="onChangePage('about')">女子：中級</button>
+        <button @click="onChangePage('about')">女子：初級</button>
+      </div>
+    </body>
 </template>
+<script setup>
+defineProps(['onChangePage'])
+</script>
+<style>
+#app {
+  font-family: 'Arial';
+  padding: 20px;
+}
+</style>
