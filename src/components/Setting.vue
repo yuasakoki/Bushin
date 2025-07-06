121f8a71f37ca9740b8dd2f0cdd6bf9ee8b532ec
yuasakoki <autoshopsanshin@gmail.com>
2025-07-06 15:00:14 +0900

    管理者メニューの追加

---
+++ src/components/Setting.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <p>管理者メニュー</p> 
+    </header>
+  <div id="settings">
+    <button @click="onChangePage('nameRecord')">選手登録</button>
+  </div>
+</template>
+<script setup>
+defineProps(['onChangePage'])
+</script>
+<style>
+#app {
+  font-family: 'Arial';
+  padding: 20px;
+}
+</style>
