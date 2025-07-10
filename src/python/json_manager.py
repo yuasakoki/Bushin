f9596a2f427fc1dbcd2399675a55a7b8c0342d7c
yuasakoki <autoshopsanshin@gmail.com>
2025-07-11 00:52:27 +0900

    ファイル名変更

--- src/python/json_manager.py
+++ src/python/json_manager.py
@@ -1,5 +1,7 @@
 # src/python/storage_manager.py
+# google-cloudとの連携を行うためのクラス
 import json
+from google.cloud import firestore
 from google.cloud import storage
 
 BUCKET_NAME = 'bushin-json'  # ご自身のバケット名
