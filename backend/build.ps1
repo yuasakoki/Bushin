ab8012eaf5753831312f591037e6960f9e9a2727
yuasakoki <autoshopsanshin@gmail.com>
2025-07-11 22:45:14 +0900

    ビルドバッチの修正

--- backend/build.ps1
+++ backend/build.ps1
@@ -1,12 +1,11 @@
 # build.ps1
-
 $rootPath = Resolve-Path "$PSScriptRoot\.."
-
-# 1. frontend に移動しビルド
 Set-Location -Path "$rootPath/frontend"
 npm install
 npm run build
-
-# 4. backendディレクトリへ移動し、Flask起動
+(Get-Content "$rootPath/backend/app/static/index.html") `
+    -replace 'src="./assets/', 'src="/static/assets/' `
+    -replace 'href="./assets/', 'href="/static/assets/' |
+    Set-Content "$rootPath/backend/app/templates/index.html"
 Set-Location -Path "$rootPath/backend"
 python -m app.app
